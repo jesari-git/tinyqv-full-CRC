98fdd64cefcf9c0dbd77136224f97b443f19979f
jesari-git <158497071+jesari-git@users.noreply.github.com>
2025-07-21 19:35:27 +0200

    test.py

--- test/test.py
+++ test/test.py
@@ -27,46 +27,18 @@ async def test_project(dut):
 
     dut._log.info("Test project behavior")
 
-    # Test register write and read back
-    await tqv.write_word_reg(0, 0x12345678)
-    assert await tqv.read_byte_reg(0) == 0x78
-    assert await tqv.read_hword_reg(0) == 0x5678
-    assert await tqv.read_word_reg(0) == 0x12345678
-
     # Set an input value, in the example this will be added to the register value
-    dut.ui_in.value = 30
+    #dut.ui_in.value = 2
+
+    # Test register write ID, DATA0, DATA1, DLCF
+    await tqv.write_word_reg(0x4, 0x10210000) 	# Polynomial  
+    await tqv.write_word_reg(0x0, 0x0)  	# Initial value
+    await tqv.write_word_reg(0x8, 0x125555ff)  	# data (32 bits)
+    await ClockCycles(dut.clk, 33)
+    await tqv.write_byte_reg(0x8, 0x00100108)   # data (8 bits)
+    await ClockCycles(dut.clk, 9)
+    assert await tqv.read_word_reg(0) == 0x49AE0000 # CRC result
 
     # Wait for two clock cycles to see the output values, because ui_in is synchronized over two clocks,
     # and a further clock is required for the output to propagate.
-    await ClockCycles(dut.clk, 3)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 0x96
-
-    # Input value should be read back from register 1
-    assert await tqv.read_byte_reg(4) == 30
-
-    # Zero should be read back from register 2
-    assert await tqv.read_word_reg(8) == 0
-
-    # A second write should work
-    await tqv.write_word_reg(0, 40)
-    assert dut.uo_out.value == 70
-
-    # Test the interrupt, generated when ui_in[6] goes high
-    dut.ui_in[6].value = 1
-    await ClockCycles(dut.clk, 1)
-    dut.ui_in[6].value = 0
-
-    # Interrupt asserted
-    await ClockCycles(dut.clk, 3)
-    assert dut.uio_out[0].value == 1
-
-    # Interrupt doesn't clear
-    await ClockCycles(dut.clk, 10)
-    assert dut.uio_out[0].value == 1
-    
-    # Write bottom bit of address 8 high to clear
-    await tqv.write_byte_reg(8, 1)
-    assert dut.uio_out[0].value == 0
+    await ClockCycles(dut.clk, 30)
